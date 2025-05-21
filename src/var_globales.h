41d1d512c4cc36bebf4659a27490805e99cc576a
Pierre Ramos <prj1@free.fr>
2025-05-21 18:18:30 +0200

    Réorganisation code

---
+++ src/var_globales.h
@@ -0,0 +1,30 @@
+/*************************************
+ *									 *
+ * DECLARATION DE VARIABLES GLOBALES *
+ *         (var_globales.h)          *
+ *									 *
+ *************************************/
+
+/* Variables globales necessaires � tout le jeu */
+extern Param parametres;
+
+extern int** matriceG;
+extern int** matriceTempG;
+
+extern Souche* soucheG;
+extern Souche* soucheTempG;
+
+extern int nbSouchesG;
+extern int stationnaire;
+
+/* variables globales necessaires � l'interface graphique */
+extern int idTimeOut;
+
+extern int xGener;
+extern int yGener;
+extern int soucheGener;
+extern int nbSouchesGener;
+extern int** matriceGener;
+
+extern int jeuEnCours;
+extern int generEnCours;
